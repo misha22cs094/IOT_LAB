46db32ee39e39614cd18e820c97174a8a71898d5
misha22cs094 <misha.22cs094@sode-edu.in>
2025-09-08 15:26:47 +0530

    Add files via upload
    Soil moisture sensor.

--- lab4.ino
+++ lab4.ino
@@ -1,29 +1,32 @@
-// C++ code
-//
-int sensor=0;
-void setup()
-{
-  pinMode(A0, INPUT);
-  pinMode(5, OUTPUT);
-  pinMode(7, OUTPUT);
-  Serial.begin(9600);
+const int sensorPin = A0;        
+const int ledPin = 13;           
+int sensorValue = 0;             
+int threshold = 600; 
+int dry=800;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);          
 }
 
-void loop()
-{
-  sensor=analogRead(A0);
-  Serial.print("Monitored value by gas sensor:");
-  Serial.println(sensor);
-  if(sensor>100){
-    digitalWrite(5, HIGH);
-  }else{
-  digitalWrite(5, LOW);
-}
-  if(sensor>120){
-    digitalWrite(7, HIGH);
-  }else{
-  digitalWrite(7, LOW);
+void loop() {
+  sensorValue = analogRead(sensorPin);  
+  Serial.print("Soil moisture sensor value: ");
+  Serial.println(sensorValue);
+
+ 
+  if (sensorValue >=dry) {
+    digitalWrite(ledPin, HIGH);
+    Serial.println("Soil is dry");
   }
-  delay(100);
-}
+    else if(sensorValue>=threshold && sensorValue<dry){
+     digitalWrite(ledPin, LOW); 
+      Serial.println("Soil is moderate");
     
+  } else {
+    digitalWrite(ledPin, LOW);    
+      Serial.println("Soil is extremely wet");
+  }
+
+  delay(1000); 
+}
